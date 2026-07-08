758403d91da7f39240c812192fb75f40103f92da
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-07-07 18:52:11 -0700

    actualiza estructura y clases en rama Kevin

---
+++ ProyectoFinal_EDD_260961/src/entidades/Estudiante.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import estructuras.VectorDinamico;
+
+/**
+ *
+ * @authors Juan Jose Meza Guarista 260961, Kevin Mendoza 215752
+ */
+public class Estudiante implements Comparable<Estudiante> {
+
+    private String matricula;
+    private String nombre;
+    private String numTelefono;
+    private String correo;
+    private String direccion;
+    private VectorDinamico<Double> calificaciones;
+
+    public Estudiante(String matricula, String nombre, String numTelefono, String correo, String direccion) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.numTelefono = numTelefono;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.calificaciones = new VectorDinamico<>(Double.class);
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public VectorDinamico<Double> getCalificaciones() {
+        return calificaciones;
+    }
+
+    @Override
+    public int compareTo(Estudiante otro) {
+        return this.matricula.compareTo(otro.getMatricula());
+    }
+
+    public void imprimirDatos() {
+        System.out.println(matricula + " - " + nombre + " - " + numTelefono);
+    }
+}
